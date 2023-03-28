ae531c85a6c7dbfafd3c14fd2f1c5b1059e83800
Michael <mherman315@gmail.com>
2023-03-28 10:57:43 -0700

    MISSION SUCCESS!
    The game is functioning but I still need to zhuzh it up with CSS, displaying counters, and work on the "Further Study" section.

--- memory-game.js
+++ memory-game.js
@@ -1,4 +1,4 @@
-'use strict';
+('use strict');
 
 /** Memory game: find matching pairs of cards and flip both of them. */
 
@@ -57,33 +57,35 @@ function createCards(colors) {
 }
 let flipCount = 0;
 let attempts = 0;
+let matchCount = 0;
+const maxMatches = colors.length / 2;
+let okToFlip = true;
 let firstFlip; // first card (div)
 let secondFlip; // second card (div)
 let color1; // first card color (string)
 let color2; // second card color (string)
-
+if (flipCount >= 2) {
+  flipCount = 0;
+}
 /** Flip a card face-up. */
 
 function flipCard(card) {
-  if (secondFlip === undefined) {
-    let color = card.classList.value;
-    return (card.style.backgroundColor = color);
-  }
-  while (flipCount === 2) {
-    if (color1 !== color2) {
-      flipCount = 0;
-      unFlipCards(firstFlip);
-    } else {
-      flipCount = 0;
-    }
-  }
+  let color = card.classList.value;
+  card.style.backgroundColor = color;
+  removeListener(card);
 }
 
 /** Flip a card face-down. */
 
-function unFlipCards(card) {
-  console.log('changing background color of firstFlip');
-  card.style.backgroundColor = 'white';
+function unFlipCards(card1, card2) {
+  let cards = [card1, card2];
+  for (let card of cards) {
+    card.style.backgroundColor = 'white';
+    addListener(card);
+    okToFlip = true;
+  }
+  firstFlip = '';
+  secondFlip = '';
 
   // console.log('changing background color of secondFlip');
   // secondFlip.style.backgroundColor = 'white';
@@ -92,19 +94,38 @@ function unFlipCards(card) {
 /** Handle clicking on a card: this could be first-card or second-card. */
 
 function handleCardClick(evt) {
-  flipCount++;
   attempts++;
-  if (flipCount === 1) {
-    flipCard(evt.target);
-    firstFlip = evt.target;
-    color1 = evt.target.classList.value;
-  } else if (flipCount === 2) {
-    flipCard(evt.target);
-    secondFlip = evt.target;
-    color2 = evt.target.classList.value;
+  if (okToFlip) {
+    if (flipCount === 0) {
+      flipCard(evt.target);
+      firstFlip = evt.target;
+      color1 = evt.target.classList.value;
+      flipCount++;
+    } else if (flipCount === 1) {
+      flipCard(evt.target);
+      secondFlip = evt.target;
+      color2 = evt.target.classList.value;
+
+      flipCount = 0;
+
+      if (color1 === color2) {
+        firstFlip = '';
+        secondFlip = '';
+        matchCount++;
+      } else {
+        okToFlip = false;
+        setTimeout(unFlipCards, 1000, firstFlip, secondFlip);
+      }
+    }
   }
 }
+function removeListener(target) {
+  target.removeEventListener('click', handleCardClick);
+}
 
+function addListener(target) {
+  target.addEventListener('click', handleCardClick);
+}
 //  Save score  ----------------
 
 // if ('lowestScore' in localStorage){
